6e098eaa475142a2f5e220565d43380bb3f96ab9
Mohammed Saif Ali <44836109+mohammedsaifali@users.noreply.github.com>
2023-01-04 10:59:29 +0400

    update with light and deep search

--- app.py
+++ app.py
@@ -183,9 +183,142 @@ st.title('CP Lead Extraction')
 #st.image('./header.jpg')
 st.write('Powered by Constructions Platform LLC')
 query = st.text_input('Enter your Keyword for lead extraction')
+type = st.selectbox('Select Algorithm?',('Light_Search', 'Deep_Search'))
 count = st.slider('Enter count of websites you want to extract leads from')
 if st.button('submit'):
-    urls = scrape_google(query)
-    for i in range(0,count):
-        crawl = EmailCrawler(urls[i])
-        crawl.crawl()
+    if type=='Deep_Search':
+        urls = scrape_google(query)
+        for i in range(0,count):
+            crawl = EmailCrawler(urls[i])
+            crawl.crawl()
+    else:
+        def get_urls(tag, n, language):
+            for i in range (n):
+                urls = search(tag, num_results=i, lang=language)
+            return urls
+
+        def remove_dup_email(x):
+            return list(dict.fromkeys(x))
+
+        def remove_dup_phone(x):
+            return list(dict.fromkeys(x))
+
+        def get_email(html):
+            try:
+                email = re.findall("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",html)
+                nodup_email = remove_dup_email(email)
+                return nodup_email
+            except:
+                 pass
+
+        def get_phone(html):
+            try:
+                phone = re.findall(r"(\d{2} \d{3,4} \d{3,4})", html)
+                phone1= re.findall(r"((?:\d{2,3}|\(\d{2,3}\))?(?:\s|-|\.)?\d{3,4}(?:\s|-|\.)\d{4})",html)
+                for p in phone1:
+                    phone.append(p)
+                nodup_phone = remove_dup_phone(phone)
+                return nodup_phone
+            except:
+                pass
+        urls = get_urls(query, count, 'en')
+
+        for url in urls:
+            res = requests.get(url,headers=headers)
+            st.write('searched home url',res.url)
+            if res.status_code == 429:
+                time.sleep(int(response.headers["Retry-After"]))
+
+        # parse the response
+        
+            info = BeautifulSoup(res.text,'lxml')
+
+
+        # extract contact data from home url
+
+            emails_home = get_email(info.get_text())
+            phones_home = get_phone(info.get_text())
+
+            emails_f = emails_home
+            phones_f = phones_home
+
+        
+        # create a data structure to store the contacts
+
+            contacts_f = {'website':res.url,'Email':'','Phone':''}
+
+        # extract contact of the link if available
+        try:
+            contact = info.find('a', text = re.compile('contact', re.IGNORECASE))['href']
+            if 'http' in contact:
+                contact_url = contact
+            else:
+                contact_url = res.url[0:-1] + contact
+
+            # searching contact URL
+            
+            res_contact = requests.get(contact_url)
+
+            contact_info = BeautifulSoup(res_contact.text, 'lxml').get_text()
+
+
+            st.write('searched contact url:', res_contact.url)
+
+            # extract contact data
+            
+            emails_contact = get_email(contact_info)
+            phones_contact = get_phone(contact_info)
+
+            #combining email contacts and email home into a single list
+
+            emails_f = emails_home
+
+            for ele1 in emails_contact:
+                emails_f.append(ele1)
+
+            #combining phone contacts and phone contacts into a single list
+        
+            phones_f = phones_home
+
+            for ele2 in phones_contact:
+                phones_f.append(ele2)
+            
+        except:
+            pass
+
+
+                
+        # removing duplicates
+
+        emails_f = remove_dup_email(emails_f)
+        phones_f = remove_dup_email(phones_f)
+
+        contacts_f['Email']= emails_f
+        contacts_f['Phone']= phones_f
+        
+        # converting into a data set
+        
+        st.write('\n', json.dumps(contacts_f, indent=2))
+
+        # dumping the data into the csv file
+
+        with open('organization_info.csv', 'a') as f:
+
+            #creater csv writer object
+
+            writer = csv.DictWriter(f, fieldnames=contacts_f.keys())
+
+            #writer.writeheader()
+
+            #append rows to the csv
+
+            writer.writerow(contacts_f)
+        df = pd.read_csv('organization_info.csv')
+        st.write(df)
+        def get_table_download_link_csv(df):
+            csv = df.to_csv().encode()
+            b64 = base64.b64encode(csv).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="captura.csv" target="_blank">Download csv file</a>'
+            return href
+        st.markdown(get_table_download_link_csv(df), unsafe_allow_html=True)
+            
